21904ecff7eff5d9ec26a179c0f070491bfd5566
Lamis Beirkdar <lamis.beirkdar@statista.com>
2025-11-07 05:56:56 +0100

    improve admin to show choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text', 'question', 'votes']
+    list_filter = ['question']
 
 admin.site.register(Question)
+admin.site.register(Choice, ChoiceAdmin)
